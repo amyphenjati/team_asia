91d63db4ab7a3697dd58c840dad448ae11179b77
amyphenji <77630330+amyphenjati@users.noreply.github.com>
2021-04-25 13:40:09 +0700

    edit respond

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-TOKEN = '1733137548:AAEMqq-UKBhfVcLD-ReXzHtQW49eJmIoEBA'
+TOKEN = "1733137548:AAEMqq-UKBhfVcLD-ReXzHtQW49eJmIoEBA"
 
 """
 Telegram bot for basic mental health check.
@@ -7,25 +7,33 @@ import logging
 from telegram import *
 from telegram.ext import *
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 logger = logging.getLogger(__name__)
+
 
 def start(update: Update, _: CallbackContext) -> None:
     """Inform user about what this bot can do"""
-    update.message.reply_text(f'Hello, I am your personal consultant. Your mental health is very important, so let check in on how you are doing! We will now be asking you 10 yes/no questions. Type /poll to continue or /help to learn more.')
+    update.message.reply_text(
+        f"Hello, I am your personal consultant. Your mental health is very important, so let check in on how you are doing! We will now be asking you 10 yes/no questions. Type /poll to continue or /help to learn more."
+    )
+
 
 def poll(update: Update, context: CallbackContext) -> None:
     """Sends a predefined poll"""
-    question_list = ["1. I have lost interest in things I used to like to do.", 
-    "2. I feel hopeless about my future.",
-    "3. I find it more difficult than it should be to make decisions.",
-    "4. I feel sluggish and often get tired for no reason.",
-    "5. I am gaining or losing weight without intending to.",
-    "6. I feel helpless to make changes I’d like to make in my life.",
-    "7. I am sleeping too much, or too little.",
-    "8. I often feel unhappy or sad, or easily cry.",
-    "9. I easily become irritable or anxious.",
-    "10. I think about dying or killing myself."]
+    question_list = [
+        "1. I have lost interest in things I used to like to do.",
+        "2. I feel hopeless about my future.",
+        "3. I find it more difficult than it should be to make decisions.",
+        "4. I feel sluggish and often get tired for no reason.",
+        "5. I am gaining or losing weight without intending to.",
+        "6. I feel helpless to make changes I’d like to make in my life.",
+        "7. I am sleeping too much, or too little.",
+        "8. I often feel unhappy or sad, or easily cry.",
+        "9. I easily become irritable or anxious.",
+        "10. I think about dying or killing myself.",
+    ]
     questions = ["Yes", "No"]
     for i in range(10):
         message = context.bot.send_poll(
@@ -46,8 +54,11 @@ def poll(update: Update, context: CallbackContext) -> None:
         }
         context.bot_data.update(payload)
 
+
 all_data = {}
 total_num_ans = {}
+
+
 def receive_poll_answer(update: Update, context: CallbackContext) -> None:
     """Summarize a users poll vote"""
     answer = update.poll_answer
@@ -62,34 +73,45 @@ def receive_poll_answer(update: Update, context: CallbackContext) -> None:
     answer_string = ""
     global all_data
     global total_num_ans
-    if answer['user']['id'] not in all_data:
-        all_data[answer['user']['id']] = 0 
+    if answer["user"]["id"] not in all_data:
+        all_data[answer["user"]["id"]] = 0
     if questions[selected_options[0]] == "Yes":
-        all_data[answer['user']['id']] = all_data.get(answer['user']['id'],0) + 1
-    total_num_ans[answer['user']['id']] = total_num_ans.get(answer['user']['id'],0) + 1
-    if total_num_ans[answer['user']['id']] == 10 and all_data[answer['user']['id']]/10.0 > 0.3:
+        all_data[answer["user"]["id"]] = all_data.get(answer["user"]["id"], 0) + 1
+    total_num_ans[answer["user"]["id"]] = total_num_ans.get(answer["user"]["id"], 0) + 1
+    if (
+        total_num_ans[answer["user"]["id"]] == 10
+        and all_data[answer["user"]["id"]] / 10.0 > 0.3
+    ):
         context.bot.send_message(
             context.bot_data[poll_id]["chat_id"],
-            f"All done, {update.effective_user.mention_html()}. You have answered yes {all_data[answer['user']['id']]} questions. If you have felt this way most every day for several weeks, you may be experiencing depression and should seek a full assessment by a psychiatrist, mental health counselor or other health care professional. If you answered yes to question 10, you should seek help immediately, regardless of your answer to any other questions.", 
-            parse_mode=ParseMode.HTML,)
-    elif total_num_ans[answer['user']['id']] == 10:
-        context.bot.send_message(context.bot_data[poll_id]["chat_id"],
-            f"Seems like you are doing just fine, {update.effective_user.mention_html()}. Have a nice day!", # prints dictionary that collects reponse in all_data
-            parse_mode=ParseMode.HTML,)
+            f"All done, {update.effective_user.mention_html()}. You have answered yes {all_data[answer['user']['id']]} questions. If you have felt this way most every day for several weeks, you may be experiencing depression and should seek a full assessment by a psychiatrist, mental health counselor or other health care professional. If you answered yes to question 10, you should seek help immediately, regardless of your answer to any other questions.",
+            parse_mode=ParseMode.HTML,
+        )
+    elif total_num_ans[answer["user"]["id"]] == 10:
+        context.bot.send_message(
+            context.bot_data[poll_id]["chat_id"],
+            f"{update.effective_user.mention_html()}, you are problably stressed. Don't worry, try meditating! BUT if you answered 'yes' to question 10, you should seek help immediately, regardless of your answer to any other questions.",  # prints dictionary that collects reponse in all_data
+            parse_mode=ParseMode.HTML,
+        )
+
 
 def help_handler(update: Update, _: CallbackContext) -> None:
-    """Display a help message"""
-    update.message.reply_text("Learn more at https://amyphenjati.github.io/team_asia/learn_more.html")
+    """You've answered 3 or more "yes" to these questions. If you have felt this way most every day for several weeks,
+    you may be experiencing depression and should seek a full assessment by a psychiatrist, mental health counselor or other health care professional.
+    If you answered "yes" to question 10, you should seek help immediately, regardless of your answer to any other questions."""
+    update.message.reply_text(
+        "Learn more at https://amyphenjati.github.io/team_asia/learn_more.html"
+    )
 
 
 def main() -> None:
     # Create the Updater and pass it your bot's token.
     updater = Updater(TOKEN)
     dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(CommandHandler('poll', poll))
-    dispatcher.add_handler(PollAnswerHandler(receive_poll_answer))  
-    dispatcher.add_handler(CommandHandler('help', help_handler))
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("poll", poll))
+    dispatcher.add_handler(PollAnswerHandler(receive_poll_answer))
+    dispatcher.add_handler(CommandHandler("help", help_handler))
 
     # Start the Bot
     updater.start_polling()
@@ -99,5 +121,5 @@ def main() -> None:
     updater.idle()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
