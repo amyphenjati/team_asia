ffe503bce613d571a92347221057895b81a81efb
amyphenji <77630330+amyphenjati@users.noreply.github.com>
2021-04-26 19:58:59 +0700

    update

--- tester_sammi.py
+++
@@ -1,7 +0,0 @@
-token = '1611604660:AAFjP0uF2glArw2XGJz8yNQvbucISbVrv4E'
-import telepot
-from pprint import pprint
-from telepot.loop import MessageLoop
-
-bot = telepot.Bot(token)
-print(bot.getMe())
